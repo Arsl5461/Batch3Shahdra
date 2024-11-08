7457e8427d7c3ec3b710b0e2ad7b898f666ff768
Zain2418418 <zainulabedeen2418@gmail.com>
2024-11-08 11:22:56 +0500

    first commit

---
+++ Backend/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose=require("mongoose")
+
+const userSchema=mongoose.Schema({
+    name:{
+        type:String
+    },
+    age:{
+        type:Number
+    },
+    email:{
+        type:String
+    },
+    password:{
+        type:String
+    }
+})
+module.exports=mongoose.model("User",userSchema)
